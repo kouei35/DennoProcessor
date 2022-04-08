bedf97445c554dcae8a8397e9f69e3515faf147b
kouei35 <kouei35@gmail.com>
2022-04-08 13:18:05 +0900

    Complete IDEX

--- ID:EX/ID:EX_test.v
+++ ID:EX/ID:EX_test.v
@@ -22,8 +22,8 @@ wire DmemWEB;
 
 /*ALU*/
 wire [3:0] ALUControl;
-wire [1:0] ALUSourceA;
-wire [2:0] ALUSourceB;
+wire ALUSourceA;
+wire [1:0] ALUSourceB;
 
 /*IDEX mux*/
 wire LoadStoremuxsel;
@@ -50,17 +50,68 @@ wire [31:0] PC;
 wire [31:0] LoadStoreOrjalAddress;
 wire [31:0] auipcOrlui;
 
-ControlUnit ControlUnit(.Opecode(Inst[31:25]),.ALUOp(Inst[14:12]),.funct(Inst[6:0]),.Dmem1ALUOUT(Dmem1ALUOUT),.DmemREB(DmemREB),.DmemWEB(DmemWEB),.ALUControl(ALUControl),.ALUSourceA(ALUSourceA),.ALUSourceB(ALUSourceB),.LoadStoremuxsel(LoadStoremuxsel),.mux2sel(mux2sel));
-regfile regfile(.rs1(Inst[19:15]),.rs2(Inst[24:20]),.rd(Inst[11:7]),.CLK(CLK),.RegWrite(RegWrite),.indata(regfile_indata),.rs1_value(Inrs1_value),.rs2_value(Inrs2_value));
-LoadStoremux LoadStoremux(.Load12Address(Inst[31:20]),.Store12Address({Inst[31:25],Inst[11:7]}),.LoadStoremuxsel(LoadStoremuxsel),.LoadStore12Address(LoadStore12Address));
-twelve2thirtytwosext twelve2thirtytwosext(.LoadStore12Address(LoadStore12Address),.LoadStore32Address(LoadStore32Address));
-twenty2thirtytwosext twenty2thirtytwosext(.InstUpper20(Inst[31:12]),.InstUpper20ext(InstUpper20ext));
-mux2 mux2(.LoadStore32Address(LoadStore32Address),.jal32(InstUpper20ext),.mux2sel(mux2sel),.LoadStoreOrjal(LoadStoreOrjal));
-shifter1 shifter1(.LoadStoreOrjal(LoadStoreOrjal),.LoadStoreOrjalAddress(InLoadStoreOrjalAddress));
-shifter12 shifter12(.InstUpper20ext(InstUpper20ext),.auipcOrlui(InauipcOrlui));
-IDEX IDEX(.CLK(CLK),.InPC(InPC),.Inrs1val(Inrs1_value),.Inrs2val(Inrs2_value),.InLoadStoreOrjalAddress(InLoadStoreOrjalAddress),.InauipcOrlui(InauipcOrlui),.PC(PC),.rs1val(rs1_value),.rs2val(rs2_value),.LoadStoreOrjalAddress(LoadStoreOrjalAddress),.auipcOrlui(auipcOrlui));
+ControlUnit ControlUnit(.Opecode(Inst[31:25]),
+                        .ALUOp(Inst[14:12]),
+                        .funct(Inst[6:0]),
+                        .Dmem1ALUOUT(Dmem1ALUOUT),
+                        .DmemREB(DmemREB),
+                        .DmemWEB(DmemWEB),
+                        .ALUControl(ALUControl),
+                        .ALUSourceA(ALUSourceA),
+                        .ALUSourceB(ALUSourceB),
+                        .LoadStoremuxsel(LoadStoremuxsel),
+                        .mux2sel(mux2sel));
 
-IFIDBlock IFIDBlock(.CLK(CLK),.RSTB(RSTB),.CNTEN(CNTEN),.PCsel(PCsel),.JumporBranch(JumporBranch),.IFIDOUTInst(Inst),.IFIDOUTPC(InPC),.ALUSourceA(ALUSourceA),.ALUSourceB(ALUSourceB));
+regfile regfile(.rs1(Inst[19:15]),
+                .rs2(Inst[24:20]),
+                .rd(Inst[11:7]),
+                .CLK(CLK),
+                .RegWrite(RegWrite),
+                .indata(regfile_indata),
+                .rs1_value(Inrs1_value),
+                .rs2_value(Inrs2_value));
+
+LoadStoremux LoadStoremux(.Load12Address(Inst[31:20]),
+                          .Store12Address({Inst[31:25],Inst[11:7]}),
+                          .LoadStoremuxsel(LoadStoremuxsel),
+                          .LoadStore12Address(LoadStore12Address));
+
+twelve2thirtytwosext twelve2thirtytwosext(.LoadStore12Address(LoadStore12Address),
+                                          .LoadStore32Address(LoadStore32Address));
+
+twenty2thirtytwosext twenty2thirtytwosext(.InstUpper20(Inst[31:12]),
+                                          .InstUpper20ext(InstUpper20ext));
+
+mux2 mux2(.LoadStore32Address(LoadStore32Address),
+          .jal32(InstUpper20ext),
+          .mux2sel(mux2sel),
+          .LoadStoreOrjal(LoadStoreOrjal));
+
+shifter1 shifter1(.LoadStoreOrjal(LoadStoreOrjal),
+                  .LoadStoreOrjalAddress(InLoadStoreOrjalAddress));
+
+shifter12 shifter12(.InstUpper20ext(InstUpper20ext),
+                    .auipcOrlui(InauipcOrlui));
+
+IDEX IDEX(.CLK(CLK),
+          .InPC(InPC),
+          .Inrs1val(Inrs1_value),
+          .Inrs2val(Inrs2_value),
+          .InLoadStoreOrjalAddress(InLoadStoreOrjalAddress),
+          .InauipcOrlui(InauipcOrlui),.PC(PC),
+          .rs1val(rs1_value),.rs2val(rs2_value),
+          .LoadStoreOrjalAddress(LoadStoreOrjalAddress),
+          .auipcOrlui(auipcOrlui));
+
+IFIDBlock IFIDBlock(.CLK(CLK),
+                    .RSTB(RSTB),
+                    .CNTEN(CNTEN),
+                    .PCsel(PCsel),
+                    .JumporBranch(JumporBranch),
+                    .IFIDOUTInst(Inst),
+                    .IFIDOUTPC(InPC),
+                    .ALUSourceA(ALUSourceA),
+                    .ALUSourceB(ALUSourceB));
 
 always begin
     #10 CLK <= ~CLK;
