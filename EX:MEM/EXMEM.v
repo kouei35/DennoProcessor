370562ab384aca333d9626ed9ca710816fbb72d9
kouei35 <kouei35@gmail.com>
2022-04-09 12:53:24 +0900

    almost comp EXMEM.v

--- EX:MEM/EXMEM.v
+++ EX:MEM/EXMEM.v
@@ -1,8 +1,39 @@
 module EXMEM(
+    CLK,
+
+    InALUOUT,
+    InZ_flag,
+    InPC,
+    InDmemREB,
+    InDmemWEB,
+    InEXMEMrd,
+
     ALUOUT,
     Z_flag,
     PC,
     DmemREB,
     DmemWEB,
-    
+    EXMEMrd,
 );
+
+input CLK;
+input [31:0] InALUOUT;
+input [31:0] InPC;
+input InDmemREB;
+input InDmemWEB;
+input [4:0] InEXMEMrd;
+
+output wire [31:0] ALUOUT;
+output wire [31:0] PC;
+output wire DmemREB;
+output wire DmemWEB;
+output wire [4:0] EXMEMrd;
+
+always @(posedge CLK) begin
+    ALUOUT <= InALUOUT;
+    PC <= InPC;
+    DmemREB <= InDmemREB;
+    DmemWEB <= InDmemWEB;
+    EXMEMrd <= InEXMEMrd;
+end
+endmodule
